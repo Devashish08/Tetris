f133c980262becaeeff6d2b63dca4764ad9c6620
Devashish08 <devashish.cs025@gmail.com>
2024-08-17 22:08:28 +0530

    Game loop

--- Main/GamePanel.java
+++ Main/GamePanel.java
@@ -3,9 +3,13 @@ package Main;
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
     public static final int WIDTH = 1280;
     public static final int HEIGHT = 720;
+    final int FPS = 60;
+    Thread gameThread;
+
+
 
     public  GamePanel() {
         this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
@@ -13,4 +17,32 @@ public class GamePanel extends JPanel {
         this.setLayout(null);
     }
 
+    public void launchGame() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+//  in every game loop we have two main methods: update and render
+    @Override
+    public void run() {
+       double drawInterval = System.nanoTime();
+       double delta = 0;
+       long lastTime = System.nanoTime();
+       long currentTime;
+
+       while(gameThread != null) {
+           currentTime = System.nanoTime();
+           delta += (currentTime - lastTime) / drawInterval;
+           lastTime = currentTime;
+              if(delta >= 1) {
+                update();
+                repaint();
+                delta--;
+              }
+       }
+    }
+
+    public void update() {}
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+    }
 }
